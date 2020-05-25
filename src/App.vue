34441ed3c4efcfa3cf7ee393860b7374a698577f
Kaje <59009825+KajeA@users.noreply.github.com>
2020-05-25 10:14:48 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <p>
+      Testing
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./styles/main.scss";
+
+
+  body {
+    background: blue;
+    color: $primary;
+  
+  }
+  
+
+</style>
