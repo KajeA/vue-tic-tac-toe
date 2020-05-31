c9389ffae52c5093399ecaa8a65f35672ca3a33d
Kaje <59009825+KajeA@users.noreply.github.com>
2020-05-30 19:39:45 -0600

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id='app'>
     <p>
       Testing
     </p>
@@ -17,15 +17,60 @@ export default {
 }
 </script>
 
-<style lang="scss">
-@import "./styles/main.scss";
+<style lang='scss'>
+@import './styles/main.scss';
 
 
   body {
-    background: blue;
+    background: $tertiary;
     color: $primary;
-  
+    font-family: $font-primary;
+    text-align: center;
+    margin: 0;
   }
   
+  #app {
+    margin: 0 auto;
+    max-width: 30em;
+    color: $primary;
+  }
+
+
+
+  // Buttons
+
+  .restart.btn {
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    background-color: $secondary;
+    color: $primary;
+    @extend .btn;
+    &:hover {
+      color: $secondary;
+    }
+  }
+
+  // Board
+
+  .board {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 2em;
+    background-color: $primary;
+    overflow-x: none;
+    padding: 1em;
+      h2 {
+        margin: 0;
+      }
+      span {
+        float: right;
+        font-size: 1.5em;
+        font-weight: 700;
+        margin-left: 2em;
+      }
+  }
 
 </style>
--- src/components/board.vue
+++ src/components/board.vue
@@ -8,6 +8,32 @@
     }
 </script>
 
-<style>
+<style lang='scss'>
+@import './styles/main.scss';
+
+.grid {
+    background-color: $primary;
+    color: $secondary;
+    width: 100%;
+}
+
+.gameStatus {
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    background-color: $secondary;
+    color: $primary;
+}
+
+.statusTurn {
+    background-color: $secondary;
+}
+
+.statusWin {
+    background-color: green;
+}
+
+.statusDraw {
+    background-color: yellow;
+}
 
 </style>
